381ea21f65d834b2f2a0e16780cf648ea0c0e7ff
yzhilong <yzlong99@gmail.com>
2024-02-12 23:48:45 +0800

    Update nonshared controller

--- src/controllers/non_shared_controller.py
+++ src/controllers/non_shared_controller.py
@@ -3,6 +3,7 @@ from components.action_selectors import REGISTRY as action_REGISTRY
 from .controller import Controller
 import torch as th
 
+
 class NonSharedMAC(Controller):
     def __init__(self, scheme, groups, args):
         super().__init__(scheme, groups, args)
@@ -10,14 +11,24 @@ class NonSharedMAC(Controller):
     def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
         # Only select actions for the selected batch elements in bs
         avail_actions = ep_batch["avail_actions"][:, t_ep]
-        agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
+        if self.allow_communications:
+            agent_outputs, message = self.forward(ep_batch, t_ep, test_mode=test_mode)
+        else:
+            agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
         chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
-        return chosen_actions
+        
+        if self.allow_communications:
+            return chosen_actions, message
+        else:
+            return chosen_actions
 
     def forward(self, ep_batch, t, test_mode=False):
         agent_inputs = self._build_inputs(ep_batch, t)
         avail_actions = ep_batch["avail_actions"][:, t]
         agent_outs, self.hidden_states = self.agent(agent_inputs, self.hidden_states)
+        if self.allow_communications:
+            message = agent_outs[:, :self.scheme['message']['vshape']]
+            agent_outs = agent_outs[:, -self.args.n_actions:]
 
         # Softmax the agent outputs if they're policy logits
         if self.agent_output_type == "pi_logits":
@@ -28,4 +39,8 @@ class NonSharedMAC(Controller):
                 agent_outs[reshaped_avail_actions == 0] = -1e10
 
             agent_outs = th.nn.functional.softmax(agent_outs, dim=-1)
-        return agent_outs.view(ep_batch.batch_size, self.n_agents, -1)
+
+        if self.allow_communications:
+            return agent_outs.view(ep_batch.batch_size, self.n_agents, -1), message.view(ep_batch.batch_size, self.n_agents, -1).detach()
+        else:
+            return agent_outs.view(ep_batch.batch_size, self.n_agents, -1)
--- src/modules/agents/rnn_ns_communicating_agent.py
+++ src/modules/agents/rnn_ns_communicating_agent.py
@@ -1,120 +1,41 @@
-import torch
+import torch as th
 import torch.nn as nn
 import torch.nn.functional as F
-
-class MessageGenerationNetwork(nn.Module):
-    def __init__(self, input_shape, message_shape, args, n_agents, trajectory_length=10):
-        super(MessageGenerationNetwork, self).__init__()
-        self.input_shape = input_shape
-        self.message_shape = message_shape
-        self.args = args
-        self.n_agents = n_agents
-        self.trajectory_length = trajectory_length
-
-        self.am = AttentionMechanism(self.input_shape, self.message_shape, self.args, self.n_agents, self.trajectory_length)
-        self.itgm
-
-    def forward(self, inputs, messages):
-        pass
-
-class AttentionMechanism(nn.Module):
-    def __init__(self, input_shape, message_shape, args, n_agents, trajectory_length=10):
-        super(AttentionMechanism, self).__init__()
-        self.input_shape = input_shape
-        self.tau_shape = input_shape + args.n_actions
-        self.message_shape = message_shape
-        self.args = args
-        self.n_agents = n_agents
-        self.trajectory_length = trajectory_length
-
-        # Not sure if we need to multiply by self.n_agents in the output_dim
-        self.qs = nn.Linear(message_shape*self.n_agents, self.args.hidden_dim)
-        self.ks = nn.Linear(self.tau_shape*trajectory_length, self.args.hidden_dim)
-        self.vs = nn.Linear(self.tau_shape*trajectory_length, self.args.hidden_dim)
-
-
-    def forward(self, imagined_trajectory, received_messages):
-        # need to duplicate messages for every agent to see
-        received_messages = torch.stack([received_messages.flatten()], 0)
-
-        qs = self.qs(received_messages)
-        ks = self.ks(imagined_trajectory)
-        vs = self.vs(imagined_trajectory)
-        sqrt_dk = torch.sqrt(self.args.hidden_dim)
-
-        messages = []
-        for i in range(self.n_agents):
-            unscaled_alpha = []
-            for j in range(self.trajectory_length):
-                q = torch.flatten(qs[i])
-                unscaled_alpha.append(torch.dot(q, ks[j, i]) / sqrt_dk)
-            alpha = torch.softmax(unscaled_alpha)
-            message = 0
-            for j in range(self.trajectory_length):
-                message += alpha[j] * vs[j, i]
-            messages.append(message)
-
-        # shape is (n_agents, message_size)
-        messages = torch.stack(messages)
-
-        return messages
-
-class ImaginedTrajectory(nn.Module):
-    def __init__(self, input_shape, message_shape, args, n_agents, trajectory_length=10):
-        super(ImaginedTrajectory, self).__init__()
-        self.args = args
-        self.input_shape = input_shape
-        self.message_shape = message_shape
-        self.n_agents = n_agents
-        self.trajectory_length = trajectory_length
-        # Define fa, fo and pi^i
-
-        self.fa_fc1 = nn.Linear(self.input_shape, self.args.hidden_dim)
-        self.fa_fc2 = nn.Linear(self.args.hidden_dim, self.args.n_actions*(self.n_agents-1))
-
-        self.fo_fc1 = nn.Linear(self.args.n_actions*self.n_agents + self.input_shape, self.hidden_dim)
-        self.fo_fc2 = nn.Linear(self.hidden_dim, self.input_shape)
-
-        self.pi_fc1 = nn.Linear(self.input_shape+self.message_shape, self.args.hidden_dim)
-        self.pi_fc2 = nn.Linear(self.args.hidden_dim, self.args.n_actions)
-
-    def forward(self, imagined_observation, imagined_action, received_message):
-        # How to separate out the taus
-        fa = F.relu(self.fa_fc1(imagined_observation))
-        fa = F.softmax(self.fa_fc2(fa))
-
-        fo = torch.stack([fa, [imagined_action], [imagined_observation]])
-        fo = F.relu(self.fo_fc1(fo))
-        fo = self.fo_fc1(fo)
-
-
+from modules.agents.rnn_communicating_agent import RNNCommunicatingAgent
 
 class RNNNSCommunicatingAgent(nn.Module):
-    def __init__(self, input_shape, message_shape, args, n_agents):
+    def __init__(self, input_shape, messages_shape, args, n_agents, trajectory_length=10):
         super(RNNNSCommunicatingAgent, self).__init__()
         self.args = args
         self.n_agents = n_agents
+        self.trajectory_length = trajectory_length
 
-        self.fc1 = nn.Linear(input_shape, args.hidden_dim)
-        if self.args.use_rnn:
-            self.rnn = nn.GRUCell(args.hidden_dim, args.hidden_dim)
-        else:
-            self.rnn = nn.Linear(args.hidden_dim, args.hidden_dim)
-        self.fc2 = nn.Linear(args.hidden_dim, args.n_actions)
+        self.obs_shape = input_shape // 2
+        self.messages_shape = messages_shape
+        self.agents = th.nn.ModuleList([RNNCommunicatingAgent(input_shape, messages_shape, args, n_agents, trajectory_length) for _ in range(self.n_agents)])
 
     def init_hidden(self):
         # make hidden states on same device as model
-        return self.fc1.weight.new(1, self.args.hidden_dim).zero_()
+        return th.cat([a.init_hidden() for a in self.agents])
 
     def forward(self, inputs, hidden_state):
-        x = F.relu(self.fc1(inputs))
-        h_in = hidden_state.reshape(-1, self.args.hidden_dim)
-        if self.args.use_rnn:
-            h = self.rnn(x, h_in)
+        hiddens = []
+        qs = []
+        if inputs.size(0) == self.n_agents:
+            for i in range(self.n_agents):
+                q, h = self.agents[i](inputs[i].unsqueeze(0), hidden_state[:, i])
+                hiddens.append(h)
+                qs.append(q)
+            return th.cat(qs), th.cat(hiddens).unsqueeze(0)
         else:
-            h = F.relu(self.rnn(x))
-        q = self.fc2(h)
+            for i in range(self.n_agents):
+                inputs = inputs.view(-1, self.n_agents, inputs.shape[-1])
+                q, h = self.agents[i](inputs[:, i], hidden_state[:, i])
+                hiddens.append(h.unsqueeze(1))
+                qs.append(q.unsqueeze(1))
+            return th.cat(qs, dim=-1).view(-1, q.size(-1)), th.cat(hiddens, dim=1)
 
-        # q here should contain the messages too, so it should have shape (n_agents, n_actions + message_shape)
-        return q, h
+    def cuda(self, device="cuda:0"):
+        for a in self.agents:
+            a.cuda(device=device)
 
