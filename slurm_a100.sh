964ef0bdb061e173aa2c6d1f25e8e7aadd661c73
yzhilong <yzlong99@gmail.com>
2024-03-01 10:36:03 +0800

    Update slurm scripts

--- slurm_a100.sh
+++ slurm_a100.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=epymarl
 #SBATCH --partition=long                        
 #SBATCH --cpus-per-task=6
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:a100mig:1
 #SBATCH --mem=65G                                     
 #SBATCH --time=120:00:00
 
